0f1c441b24af25a840e122cc1cb04ebbdfb381a6
Saranomac <saranomy@gmail.com>
2019-04-14 10:10:51 -0700

    change text to Git Demo 2

--- githubJavaDemo/src/githubjavademo/GithubJavaDemo.java
+++ githubJavaDemo/src/githubjavademo/GithubJavaDemo.java
@@ -16,7 +16,7 @@ public class GithubJavaDemo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Github Demo");
+        System.out.println("Github Demo 2");
     }
     
 }
