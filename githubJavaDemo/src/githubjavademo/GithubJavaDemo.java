f089a60ba4d0840637042cfc827d4b5a9b617d7b
mooredangithub <danny.moore@microsemi.com>
2019-04-14 10:32:03 -0700

    modified producdtion file

--- githubJavaDemo/src/githubjavademo/GithubJavaDemo.java
+++ githubJavaDemo/src/githubjavademo/GithubJavaDemo.java
@@ -16,7 +16,7 @@ public class GithubJavaDemo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Github Demo Production Branch");
+        System.out.println("Github Demo Production Branch again");
     }
     
 }
